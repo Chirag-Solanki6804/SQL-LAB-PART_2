c2a4b979976b60e1158db1f76a06d3012a9d25c9
chirag solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2022-07-10 11:27:11 +0530

    Update assignment1.sql

--- assignment1.sql
+++ assignment1.sql
@@ -7,96 +7,133 @@ STATEID INT NOT NULL,
 CITYREMARKS VARCHAR(500) NULL
 );
 SELECT*FROM MST_CITY;
---Q.1
-CREATE Procedure PR_MST_CITY_SELECTALL
-AS 
-BEGIN
-  SELECT
-  MST_CITY.CITYID,
-  MST_CITY.CITYNAME,
-  MST_CITY.PINCODE,
-  MST_CITY.CityRemarks, 
-  MST_CITY.StateID, 
-  MST_CITY.STATENAME
-  FROM MST_CITY
-   INNER JOIN MST_STATE
-	 ON MST_CITY.STATEID=MST_STATE.STATEID
-END
-
---Q.2  [DBO].[PR_MST_CITY_INSERT] 1,'PORBANDAR','360570',123,NULL
-CREATE Procedure PR_MST_CITY_INSERT 
-      @CityID       Int,
-      @CityName     Varchar(250),
-      @Pincode      Varchar(6),
-      @StateID      Int,
-      @CityRemarks  Varchar(500)
-AS
-BEGIN
-     INSERT INTO MST_CITY
-	 VALUES
-	 (
-	 CITYID,
-	 CITYNAME,
-	 PINCODE,
-	 STATEID,
-	 CITYREMARKS
-	 )
-END
-SELECT*FROM MST_CITY;
---Q.3 [DBO].[PR_MST_CITY_UPDATE]
-CREATE Procedure PR_MST_CITY_UPDATE
-      @CityID       Int,
-      @CityName     Varchar(250),
-      @Pincode      Varchar(6),
-      @StateID      Int,
-      @CityRemarks  Varchar(500)
-AS 
-BEGIN
-      UPDATE MST_CITY 
-	       SET
-		    @CityID      =  CITYID,
-            @CityName    =  CITYNAME,
-            @Pincode     =  PINCODE,
-            @StateID     =  STATEID,
-            @CityRemarks =  CITYREMARKS
-END
----Q.4
---[DBO].[PR_MST_CITY_DELETE]
-CREATE Procedure PR_MST_CITY_DELETE
-     @CITYID INT
-AS 
-BEGIN
-    DELETE 
-	FROM MST_CITY
-	WHERE CITYID=@CITYID
-END
---Q.5 [DBO].[PR_MST_CITY_SELECTBYPK]
-CREATE Procedure PR_MST_CITY_SELECTBYPK
-   @CITYID INT
-AS 
-BEGIN
-     SELECT*FROM
-	 MST_CITY WHERE CITYID=@CITYID
-END
----Q.6 [dbo].[SelectByPincodeStartWith360]
-CREATE Procedure PR_MST_CITY_SelectByPincodeStartWith360
-     PINCODE VARCHAR(6)
-AS 
-BEGIN
-   SELECT*FROM MST_CITY
-   WHERE PINCODE LIKE'360%';
-END
----Q.7
---[DBO].[SelectByCityNamePincode]CREATE PROCEDURE PR_MST_CITY_SelectByCityNamePincodeAS BEGINEND
-
-
-
-
-
-			 
-
-
-
-
-
-
+----------------Q.1 select All------------
+create proc PR_MST_City_SelectAll
+as
+begin
+select cityid,cityname,pincode,cityremarks,MST_city.stateid 
+from
+MST_City inner join MST_State 
+on
+MST_State.stateid=MST_City.stateid
+end
+exec PR_MST_City_SelectAll
+----------------------Q.2 Insert-----------------
+create proc PR_MST_City_Insert
+@cityid int,
+@cityname varchar(50),
+@pincode varchar(6),
+@stateid int,
+@cityremarks varchar(500)
+as
+begin
+Insert into MST_City
+(
+cityid,
+cityname,
+pincode ,
+stateid,
+cityremarks
+)
+values
+(
+@cityid,
+@cityname,
+@pincode,
+@stateid,
+@cityremarks
+)
+end
+exec PR_MST_City_Insert 105,porbandar,360570,1,'Excellent'
+----------------------------Q.3 UPDATE----------------------
+alter proc PR_MST_City_Update
+@cityid int,
+@cityname varchar(50)
+as
+begin
+update MST_City
+set
+cityname=@cityname
+where
+cityid=@cityid
+end
+exec PR_MST_City_Update 105,abcd
+--------------------Q.4 Delete----------------------------------
+create proc PR_MST_City_Delete
+@cityid int
+as
+begin
+delete from
+MST_City 
+where
+cityid=@cityid 
+end
+exec PR_MST_City_Delete 105
+----------------------Q.5 SelectByPK--------------------------
+create proc PR_MST_City_SelectByPK
+@cityid int
+as
+begin
+select *
+from MST_City 
+where
+cityid=@cityid 
+end
+exec PR_MST_City_SelectByPK 1
+-------------------Q.6 SelectByPincodeStartWith360--------------------------
+create proc SelectByPincodeStartWith360
+as
+begin
+select *
+from MST_City 
+where
+pincode like '360%'
+end
+exec SelectByPincodeStartWith360
+------------------------Q.7 SelectBycitynamePincode-------------------------
+create proc SelectBycitynamePincode
+@cityname varchar(50),
+@pincode varchar(6)
+as
+begin
+select *
+from MST_City 
+where
+cityname=@cityname and pincode=@pincode
+end
+exec SelectBycitynamePincode Rajkot,360001
+-----------------------Q.8 SelectBycityremarks----------------------
+create proc SelectBycityremarks
+@cityremarks varchar(50)
+as
+begin
+select *
+from MST_City 
+where
+cityremarks=@cityremarks
+end
+exec SelectBycityremarks good
+-------------------------Q.9 SelectBystateid------------------------
+create proc SelectBystateid
+@stateid int
+as
+begin
+select *
+from MST_City 
+where
+stateid=@stateid
+end
+exec SelectBystateid 1
+--------------------------------Q.10 SelectBycityidstateid--------------------
+create proc SelectBycityidstateid
+@stateid int,
+@cityid int
+as
+begin
+select *
+from MST_City 
+where stateid=@stateid 
+and
+cityid=@cityid
+end
+exec SelectBycityidstateid 1,1
